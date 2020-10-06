fc2c3d46a8cc296848a90f320a62b70bf2caf8ec
Maria Victoria Martinez <vickymartinezvillalba@gmail.com>
2020-10-06 17:00:05 -0300

    modelo Cinema

--- Models/Cinema.php
+++ Models/Cinema.php
@@ -1,59 +1,70 @@
-<?php
-namespace Models;
-class Cinema{
-private $name;
-private $address; 
-private $openingTime
-private $closingTime
-private $ticketValue;
-private $id;
+	<?php
+	namespace Models;
+	class Cinema{
+		private $name;
+		private $address; 
+		private $openingTime
+		private $closingTime
+		private $ticketValue;
+		private $id;
 
-public function setName($name){
-$this->name=$name;
-}
+		public function __construct(String $name, String $adress, date $openingTime, date $closingTime, String $ticketValue, int $id = 0)
+		{
+			$this->setName($name);
+			$this->setAddress($adress);
+			$this->setOpeningTime($openingTime);
+			$this->setClosingTime($closingTime);
+			$this->setTicketValue($ticketValue);
+			$this->setId($id);
 
-public function getName(){
-return $this->name;
-}
+		}
 
-public function setAddress($address){
-$this->address=$address;
-}
+		public function setName($name){
+			$this->name=$name;
+		}
 
-public function getAddress(){
-return $this->address;
-}
-public function setOpeningTime($openingTime){
-$this->openingTime=$openingTime;
-}
+		public function getName(){
+			return $this->name;
+		}
 
-public function getOpeningTime(){
-return $this->openingTime;
-}
-public function setClosingTime($closingTime){
-$this->closingTime=$closingTime;
-}
+		public function setAddress($address){
+			$this->address=$address;
+		}
 
-public function getClosingTime(){
-return $this->closingTime;
-}
+		public function getAddress(){
+			return $this->address;
+		}
+		public function setOpeningTime($openingTime){
+			$this->openingTime=$openingTime;
+		}
 
-public function setTicketValue($ticketValue){
-$this->ticketValue=$ticketValue;
-}
+		public function getOpeningTime(){
+			return $this->openingTime;
+		}
+		public function setClosingTime($closingTime){
+			$this->closingTime=$closingTime;
+		}
 
-public function getTicketValue(){
-return $this->ticketValue;
-}
+		public function getClosingTime(){
+			return $this->closingTime;
+		}
 
-public function setId($id){
-$this->id=$id;
-}
+		public function setTicketValue($ticketValue){
+			$this->ticketValue=$ticketValue;
+		}
 
-public function getId(){
-return $this->id;`
-}
+		public function getTicketValue(){
+			return $this->ticketValue;
+		}
+
+		public function setId($id){
+			$this->id=$id;
+		}
+
+		public function getId(){
+			return $this->id;`
+		}
 
 
-}
-?>
+	}
+	?>
