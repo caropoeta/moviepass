fbaa908a38c6cd46d47568fce64aa730d43d1604
Maria Victoria Martinez <vickymartinezvillalba@gmail.com>
2020-10-17 14:25:39 -0300

    elimino Modify Cinema como opcion

--- Views/indexCinema.php
+++ Views/indexCinema.php
@@ -4,7 +4,6 @@ include('navbaradmin.php');
 <main class="d-flex  justify-content-center ">
  <div class="content">
     <header class="text-center">
- 
 </header>
 <div class="login-form bg-dark-alpha p-5 bg-light">
 <section>
@@ -35,16 +34,6 @@ include('navbaradmin.php');
         <input type="text" name="deleteCinema" class="form-control" placeholder="Enter Movie name to be deleted">
         <br>
         <button type="submit" class="btn btn-primary offset-4 " required >Delete Cinema</button>
-    </div>
-</form>
-
-<form action="<?php echo FRONT_ROOT ?>Cinema/ModifyCinema" method="POST">
-    <div class="form-group">
-        <br>
-        <input type="text" name="name" placeholder="Enter Name to be modify" required class="form-control">
-        <br>
-        
-        <button type="submit" class="btn btn-primary offset-4  ">Modify Cinema</button>
     </div>
 </form>
 
