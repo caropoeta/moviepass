8e029683d534a4a0e2dd08b3654dbb4653673661
jadeDemon <56654296+jadeDemon@users.noreply.github.com>
2020-10-23 13:30:54 -0300

    Limpieza del proyecto

--- Config/Config.php
+++ Config/Config.php
@@ -9,11 +9,6 @@ define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
 define("IMG_PATH", VIEWS_PATH . "img/");
 define('DATA_PATH', 'Data/');
 
-define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'moviepass_user');
-define('DB_PASSWORD', 'moviepass@localhost');
-define('DB_NAME', 'moviepass');
-
 define('APP_ID', '383668786136123');
 define('APP_SECRET', '07cde429233190afc3f433c626dbfc0e');
 
