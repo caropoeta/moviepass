0e0031d686eb6b969b8ef65b1e5c181e222d635c
Maria Victoria Martinez <vickymartinezvillalba@gmail.com>
2020-10-25 13:50:29 -0300

    agrego metodos con acceso a bbdd delete-modify-searh-show

--- Models/Cinema.php
+++ Models/Cinema.php
@@ -1,81 +1,81 @@
 <?php
 namespace Models;
 
-	class Cinema{
-		private $name;
-		private $adress; 
-		private $openingTime;
-		private $closingTime;
-		private $ticketValue;
-		private $capacity;
-		private $id;
-		private $delete;
+class Cinema{
+	private $nameCinema;
+	private $address; 
+	private $openingTime;
+	private $closingTime;
+	private $ticketValue;
+	private $capacity;
+	private $idCinema;
+	private $deleteCinema;
 
-		public function __construct()
-		{
-		}
-
-		public function setName($name){
-			$this->name=$name;
-		}
-
-		public function getName(){
-			return $this->name;
-		}
-
-		public function setAdress($adress){
-			$this->adress=$adress;
-		}
-
-		public function getAdress(){
-			return $this->adress;
-		}
-		public function setOpeningTime($openingTime){
-			$this->openingTime=$openingTime;
-		}
-
-		public function getOpeningTime(){
-			return $this->openingTime;
-		}
-		public function setClosingTime($closingTime){
-			$this->closingTime=$closingTime;
-		}
-
-		public function getClosingTime(){
-			return $this->closingTime;
-		}
-
-		public function setTicketValue($ticketValue){
-			$this->ticketValue=$ticketValue;
-		}
-
-		public function getTicketValue(){
-			return $this->ticketValue;
-		}
-
-		public function setCapacity($capacity){
-			$this->capacity=$capacity;
-		}
-
-		public function getCapacity(){
-			return $this->capacity;
-		}
-
-		public function setId($id){
-			$this->id=$id;
-		}
-
-		public function getId(){
-			return $this->id;
-		}
-
-		public function setDelete($delete){
-			$this->delete=$delete;
-		}
-
-		public function getDelete(){
-			return $this->delete;
-		}
-
+	public function __construct()
+	{
 	}
-	?>
+
+	public function setnameCinema($nameCinema){
+		$this->nameCinema=$nameCinema;
+	}
+
+	public function getnameCinema(){
+		return $this->nameCinema;
+	}
+
+	public function setaddress($address){
+		$this->address=$address;
+	}
+
+	public function getaddress(){
+		return $this->address;
+	}
+	public function setopeningTime($openingTime){
+		$this->openingTime=$openingTime;
+	}
+
+	public function getopeningTime(){
+		return $this->openingTime;
+	}
+	public function setclosingTime($closingTime){
+		$this->closingTime=$closingTime;
+	}
+
+	public function getclosingTime(){
+		return $this->closingTime;
+	}
+
+	public function setticketValue($ticketValue){
+		$this->ticketValue=$ticketValue;
+	}
+
+	public function getticketValue(){
+		return $this->ticketValue;
+	}
+
+	public function setcapacity($capacity){
+		$this->capacity=$capacity;
+	}
+
+	public function getcapacity(){
+		return $this->capacity;
+	}
+
+	public function setidCinema($idCinema){
+		$this->idCinema=$idCinema;
+	}
+
+	public function getidCinema(){
+		return $this->idCinema;
+	}
+
+	public function setdeleteCinema($deleteCinema){
+		$this->deleteCinema=$deleteCinema;
+	}
+
+	public function getdeleteCinema(){
+		return $this->deleteCinema;
+	}
+
+}
+?>
