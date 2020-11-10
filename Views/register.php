7c1fcea2e9a303d13a0be88e51897c9cbc842097
caropoeta <66274239+caropoeta@users.noreply.github.com>
2020-11-10 09:21:45 -0300

    actualizacion register

--- Views/register.php
+++ Views/register.php
@@ -1,39 +1,39 @@
-<main class="d-flex  justify-content-center ">
-     <div class="content">
-          
+
+<main class="main ">
+     <div class="login-form">
+
           <header class="text-center">
-               <h2 class="fuente4 ">MoviePass Register Form</h2>
+               <h2>Register</h2>
           </header>
-<div class="login-form bg-dark-alpha p-5 bg-light rounded back">
-    <form action="<?php echo FRONT_ROOT ?>Session/Register" method="post" >
+               <form action="<?php echo FRONT_ROOT ?>Session/Register" method="post">
 
-               <div class="form-group">
-                    <label for="">User</label>
-                    <input type="text" name="username" class="form-control form-control-lg" placeholder="User" required>
-               </div>
-               <div class="form-group">
-                    <label for="">Password</label>
-                    <input type="password" name="password" class="form-control form-control-lg" placeholder="Password" required>
-               </div>
-               <div class="form-group">
-                    <label for="">DNI</label>
-                    <input type="number" name="dni" class="form-control form-control-lg" placeholder="DNI" required minlength="7" maxlength="8">
-               </div>
-               <div class="form-group">
-                    <label for="">Email</label>
-                    <input type="email" name="email" class="form-control form-control-lg" placeholder="Email" required>
-               </div>
-               <div class="form-group">
-                    <label for="">BirthDay</label>
-                    <input type="date" name="birthday" class="form-control form-control-lg" placeholder="BirthDay" required pattern="[0-9]{2}-[0-9]{2}-[0-9]{4}">
-               </div>
-               <button class="btn btn-primary btn-block btn-lg" type="submit">Send Registration Form</button>
-               <br>
+                    <div class="form-group">
+                         <label for="">User</label>
+                         <input type="text" name="username" class="form-control " placeholder="User" required>
+                    </div>
+                    <div class="form-group">
+                         <label for="">Password</label>
+                         <input type="password" name="password" class="form-control " placeholder="Password" required>
+                    </div>
+                    <div class="form-group">
+                         <label for="">DNI</label>
+                         <input type="number" name="dni" class="form-control " placeholder="DNI" required minlength="7" maxlength="8">
+                    </div>
+                    <div class="form-group">
+                         <label for="">Email</label>
+                         <input type="email" name="email" class="form-control" placeholder="Email" required>
+                    </div>
+                    <div class="form-group">
+                         <label for="">BirthDay</label>
+                         <input type="date" name="birthday" class="form-control" placeholder="BirthDay" required pattern="[0-9]{2}-[0-9]{2}-[0-9]{4}">
+                    </div>
+                    <button class="botons" type="submit">Send Register</button>
+                    <br>
 
-          </form>
-          <form method="post">
-           <button  formaction="<?php echo FRONT_ROOT ?>home/MainPage" class="btn btn-primary btn-block btn-lg" type="submit">Return to Index</button>
-           </form>
+               </form>
+               <form method="post">
+                    <button formaction="<?php echo FRONT_ROOT ?>home/MainPage" class="botons" type="submit">Return to Index</button>
+               </form>
+          
      </div>
-</div>
 </main>
